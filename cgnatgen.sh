bea7549a8672450e6f4e775e29c48d21466e6051
dvhoisel <danielhoisel@gmail.com>
2021-03-07 13:12:07 -0300

    Add files via upload

---
+++ cgnatgen.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+# Desenvolvido por Daniel Hoisel
+# Licenciado sob a GPL 3.0
+versao="0.1"
+while : ; do
+	entrada=$( dialog --stdout --backtitle 'cgnatgen' --title "cgnatgen - Desenvolvido por Daniel Hoisel - Versão: $versao" \
+    --inputbox "
+                Obs.: Para haver validação, o programa
+                ipcalc tem que estar instalado.
+                Confira a entrada de dados.
+                
+                Informe o bloco(pool) privado, com a máscara. Ex: 100.100.0.0/20" 0 0 )
+	if which ipcalc >/dev/null; then
+        ipcalc -cbn $entrada | grep Network | cut -f2 -d: | grep $entrada || { echo "Endereço de IP ou de rede inválidos!"; exit; }
+    else
+        echo ipcalc não instalado
+    fi
+    ipprivado=`echo $entrada | cut -f 1 -d /`
+    mascaraprivado=`echo $entrada | cut -f 2 -d /`
+    if [[ $mascaraprivado -gt 25 ]]
+    then
+        dialog --stdout --msgbox 'Quem faz CGNAT com tão poucos IPs?' 0 0
+        exit
+    fi
+    entrada=$( dialog --stdout --backtitle 'cgnatgen' \
+                --title "cgnatgen - Desenvolvido por Daniel Hoisel - Versão: $versao" \
+                --inputbox "
+                PORTAS X PREFIXO PÚBLICO NECESSÁRIO
+                -----------------------------------
+                8000: /$(( $mascaraprivado + 3 ))
+                4000: /$(( $mascaraprivado + 4 ))
+                2000: /$(( $mascaraprivado + 5 ))
+                1000: /$(( $mascaraprivado + 6 ))
+                0500: /$(( $mascaraprivado + 7 ))
+                
+                Informe o bloco(pool) público, com a máscara. Ex: 200.200.0.0/25" 0 0 )
+	if which ipcalc >/dev/null; then
+        ipcalc -cbn $entrada | grep Network | cut -f2 -d: | grep $entrada || { echo "Endereço de IP ou de rede inválidos!"; exit; }
+    else
+        echo ipcalc não instalado
+    fi
+    ippublico=`echo $entrada | cut -f 1 -d /`
+    mascarapublico=`echo $entrada | cut -f 2 -d /`
+    quantidadepublico=$((2**$((32-$mascarapublico))))
+    quantidadeprivado=$((2**$((32-$mascaraprivado))))
+    relacao=$(($quantidadeprivado/$quantidadepublico))
+    portas=$((64000/$relacao))
+    mascarajump=$((32-($mascarapublico-$mascaraprivado)))
+    echo "/ip firewall nat"> mk-cgnat.rsc
+    ippubpo=`echo $ippublico | cut -d . -f 1`
+    ippubso=`echo $ippublico | cut -d . -f 2`
+    ippubto=`echo $ippublico | cut -d . -f 3`
+    ippubqo=`echo $ippublico | cut -d . -f 4`
+    ipprvpo=`echo $ipprivado | cut -d . -f 1`
+    ipprvso=`echo $ipprivado | cut -d . -f 2`
+    ipprvto=`echo $ipprivado | cut -d . -f 3`
+    ipprvqo=`echo $ipprivado | cut -d . -f 4`
+    comecoporta=1500
+    y=1
+    while [ $y -le $quantidadepublico ]
+    do
+        if [[ $ippubqo -gt 255 ]]
+        then
+            ippubqo=0
+            ippubto=$(( $ippubto + 1))
+        fi
+        if [[ $ipprvqo -gt 255 ]]
+        then
+            ipprvqo=0
+            ipprvto=$(( $ipprvto + 1))
+        fi
+        echo "add chain=srcnat action=jump comment=\"CGNAT por cgnatgen - JUMP para $ippubpo.$ippubso.$ippubto.$ippubqo\" jump-target=\"CGNAT-$ippubpo.$ippubso.$ippubto.$ippubqo\" src-address=\"$ipprvpo.$ipprvso.$ipprvto.$ipprvqo/$mascarajump\"" >> mk-cgnat.rsc
+        ippubqo=$(( $ippubqo + 1 ))
+        ipprvqo=$(( $ipprvqo + $relacao ))
+        y=$(( $y + 1 ))
+    done
+    ippubpo=`echo $ippublico | cut -d . -f 1`
+    ippubso=`echo $ippublico | cut -d . -f 2`
+    ippubto=`echo $ippublico | cut -d . -f 3`
+    ippubqo=`echo $ippublico | cut -d . -f 4`
+    ipprvpo=`echo $ipprivado | cut -d . -f 1`
+    ipprvso=`echo $ipprivado | cut -d . -f 2`
+    ipprvto=`echo $ipprivado | cut -d . -f 3`
+    ipprvqo=`echo $ipprivado | cut -d . -f 4`
+    y=1
+    portainicial=$comecoporta
+    while [ $y -le $quantidadepublico ]
+    do
+        if [[ $ippubqo -gt 255 ]]
+        then
+            ippubqo=0
+            ippubto=$(( $ippubto + 1))
+        fi
+        if [[ $ipprvqo -gt 255 ]]
+        then
+            ipprvqo=0
+            ipprvto=$(( $ipprvto + 1))
+        fi
+        echo "add chain=\"CGNAT-$ippubpo.$ippubso.$ippubto.$ippubqo\" action=src-nat protocol=icmp src-address=$ipprvpo.$ipprvso.$ipprvto.$ipprvqo/$mascarajump to-address=$ippubpo.$ippubso.$ippubto.$ippubqo comment=\"CGNAT-$ippubpo.$ippubso.$ippubto.$ippubqo\"" >> mk-cgnat.rsc
+        x=1
+        while [ $x -le $relacao ]
+        do
+            if [[ $ippubqo -gt 255 ]]
+            then
+                ippubqo=0
+                ippubto=$(( $ippubto + 1))
+            fi
+            if [[ $ipprvqo -gt 255 ]]
+            then
+                ipprvqo=0
+                ipprvto=$(( $ipprvto + 1))
+            fi
+            echo "add chain=\"CGNAT-$ippubpo.$ippubso.$ippubto.$ippubqo\" action=src-nat protocol=tcp src-address=$ipprvpo.$ipprvso.$ipprvto.$ipprvqo to-address=$ippubpo.$ippubso.$ippubto.$ippubqo to-ports=$portainicial-$(( $portainicial + $portas - 1 )) comment=\"CGNAT-$ippubpo.$ippubso.$ippubto.$ippubqo\"" >> mk-cgnat.rsc
+            echo "add chain=\"CGNAT-$ippubpo.$ippubso.$ippubto.$ippubqo\" action=src-nat protocol=udp src-address=$ipprvpo.$ipprvso.$ipprvto.$ipprvqo to-address=$ippubpo.$ippubso.$ippubto.$ippubqo to-ports=$portainicial-$(( $portainicial + $portas - 1 )) comment=\"CGNAT-$ippubpo.$ippubso.$ippubto.$ippubqo\"" >> mk-cgnat.rsc
+            portainicial=$(( $portainicial + $portas ))
+            ipprvqo=$(( $ipprvqo + 1 ))
+            x=$(( $x + 1 ))
+        done
+        portainicial=1500
+        ippubqo=$(( $ippubqo + 1 ))
+        y=$(( $y + 1 ))
+    done
+    if [[ $portas -lt 500 || $portas -gt 8000 ]]
+    then
+        aviso1="AVISO: A quantidade mínima e máxima de portas"
+        aviso2="recomendada é 500 e 8000, respectivamente"
+    fi
+    dialog \
+                --cr-wrap \
+                --sleep 5 \
+                --backtitle 'cgnatgen'   \
+                --title #cgnatgen - Desenvolvido por Daniel Hoisel - Versão: $versao" \
+                --infobox "
+                Gerando o arquivo mk-cgnat.rsc
+
+                Quantidade de IPs públicos: $quantidadepublico
+                Quantidade de IPs privados: $quantidadeprivado
+                Relação entre público e privado: 1:$relacao
+                Quantidade de portas para cada IP privado: $portas
+                $aviso1
+                $aviso2
+                " 12 60
+    exit
+done
